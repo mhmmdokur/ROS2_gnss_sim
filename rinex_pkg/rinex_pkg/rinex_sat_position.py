5753174b468a707d29a16585526733847c32ab9b
mhmmdokur <42721446+mhmmdokur@users.noreply.github.com>
2024-03-20 23:14:18 +0300

    v0.0.2

--- rinex_pkg/rinex_pkg/rinex_sat_position.py
+++ rinex_pkg/rinex_pkg/rinex_sat_position.py
@@ -2,20 +2,75 @@
 
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import NavSatFix
 from RSP.parse_rinex import read_rinex
-from RSP.satpos import calculate_satpos, calculate_positions
+from RSP.satpos import calculate_satpos, calculate_positions, calculate_single_positions
+from Transform.geo import ecef_to_geodetic, geodetic_to_enu, ecef_to_enu
+
+
+class NavSatFixSubscriber(Node):
+    def __init__(self):
+        super().__init__('navsatfix_subscriber')
+
+        self.subscription = self.create_subscription(
+            NavSatFix,
+            '/fix',
+            self.fix_callback,
+            10)
+
+    def fix_callback(self, msg):
+        # /fix topic'inden gelen verileri işle
+        self.enlem = msg.latitude
+        self.boylam = msg.longitude
+        self.irtifa = msg.altitude
+
+class RinexPublisher(Node):
+    def __init__(self):
+        super().__init__('rinex_publisher')
+
+        # Yayın oluşturma
+        self.publisher = self.create_publisher(NavSatFix, '/rinex_data', 10)
+        # Rinex dosyasından veri okuma
+        self.sat_data = read_rinex('./gmez0380.21n')
+        # Yayın işlemi
+        self.timer = self.create_timer(1, self.publish_rinex_data)
+
+    def publish_rinex_data(self):
+        # Veri işleme ve yayın yapma işlemleri
+        #sat_pos = calculate_positions(self.sat_data)
+        
+        #first_key = next(iter(sat_pos))
+        #llh = ecef_to_geodetic(sat_pos[first_key]['x'], sat_pos[first_key]['y'], sat_pos[first_key]['z'])
+        # İlk rinex verisini almak için
+        sat_pos = calculate_single_positions(self.sat_data)
+        single_rinex_data = next(sat_pos)
+        llh = ecef_to_geodetic(single_rinex_data['x'], single_rinex_data['y'], single_rinex_data['z'])
+
+        # NavSatFix mesajını oluştur
+        msg = NavSatFix()
+        msg.latitude = llh[0]
+        msg.longitude = llh[1]
+        msg.altitude = llh[2]
+
+        # Yayın yapma
+        self.publisher.publish(msg)
 
 
 def main(args=None):
+    
     rclpy.init(args=args)
-    node = Node("rinex_test")
+    navSatFixSubsNode = NavSatFixSubscriber()
+    rinexPublisherNode = RinexPublisher()
 
-    sat_data = read_rinex('./gmez0380.21n')
-    sat_pos = calculate_positions(sat_data)
-    #print(sat_pos)
+    # Her iki Node'u da aynı anda çalıştır
+    executor = rclpy.executors.MultiThreadedExecutor(num_threads=2)
+    executor.add_node(navSatFixSubsNode)
+    executor.add_node(rinexPublisherNode)
+    executor.spin()
 
-    node.get_logger().info(sat_pos)
+    # Spin döngüsünden çıktığımızda ROS 2'yi kapat
     rclpy.shutdown()
+
 
 if __name__== "__main__":
     main()
--- rinex_pkg/setup.py
+++ rinex_pkg/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "rinex_node = rinex_pkg.rinex_sat_position:main"
         ],
     },
 )
